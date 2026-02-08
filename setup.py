c445fe9c07277666875f403dbc505146590055fe
Automated Publisher <actions@users.noreply.github.com>
2026-02-08 23:08:28 +0000

    Automated publish: Sun Feb  8 23:08:28 UTC 2026 fb40261331a1baf1bd65045043ac9d7a92ac768f

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup_kwargs = {
     "name": "lidarr-youtube-downloader",
-    "version": "0.3.33",
+    "version": "0.3.34",
     "description": "",
     "long_description": long_description,
     "long_description_content_type": "text/markdown",
